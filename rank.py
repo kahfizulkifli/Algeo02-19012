990f0ec5d3d8188b4a8be7f7eadc0e11681cc28e
kahfizulkifli <sbhnkahfi@gmail.com>
2020-11-10 10:21:30 +0700

    rank.py serta sample dokumen, baru sampai kata2 unik

--- rank.py
+++ rank.py
@@ -1,6 +1,8 @@
 # boleh nambah import atau variabel global
 # dokumen-dokumen ada di directory "docs/"
 
+query = ['Donald', 'Trump', 'lost']
+
 def GetRank(terms):
 	# GetRank(query) menerima list of terms
 	# dan mengembalikan array/list berisi tuple
@@ -8,4 +10,15 @@ def GetRank(terms):
 	# jumlah kata di dokumen, 
 	# tingkat kemiripan dokumen dengan query,
 	# kalimat pertama dari dokumen)
+
+	# semua kata di lower case dan dibuat list kata2 unik secara berurutan
+	file = open("sample.txt", "r")
+	data = file.read().lower()
+	array = data.split()
+	unique_words = list(set(array))
+	unique_words.sort()
+
+	
+	print(unique_words)
+GetRank(query)
 	
