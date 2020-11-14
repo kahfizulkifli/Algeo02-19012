dfbf262ca7d4bf85fdc0ac5531a42897d90c3e3b
akramab <70879748+akramab@users.noreply.github.com>
2020-11-14 21:16:22 +0700

    Insyaallah udah

--- terms.py
+++ terms.py
@@ -1,6 +1,9 @@
+from tabel import *
+
 # boleh nambah import atau variabel global
 
 def GetTerms(query):
+    return StopWordsRemove(StemmingKonten(StringToArray(RegexCleaning(query))))
 	# GetTerms menerima string query dari pengguna
 	# dan mengembalikan array/list berisi string
 	# term-term pada query
